19509fd113449f85cd441b73cbf880268b4652c9
Roberto Garza <robertg4567@hotmail.com>
2018-07-25 18:52:49 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -49,7 +49,7 @@ class Song
     @song = self.new
     array = fileName.split(" - ")
     @song.name = array[1].chomp(".mp3")
-    @song.artist_name = array[0].chomp(".mp3")
+    @song.artist_name = array[0]
     @song
   end
 end
