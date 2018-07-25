539655d5a42bfc4f97d259d1a5fc8b034c6a17b0
Roberto Garza <robertg4567@hotmail.com>
2018-07-25 18:32:15 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -23,7 +23,9 @@ class Song
   end
 
   def self.create_by_name(name)
-    self.new_by_name(name)
-    @@all < @song
+    @song = self.new
+    @song.name = name 
+    @@all << @song
+    @song
   end
 end
